bf2d4e287343ff70ca11ea62db66f604e0319759
Guilherme Pontes <guisantos@sp.r7.com>
2014-09-05 11:07:02 -0300

    more testinggg :punch:

--- specs/g.methods.spec.js
+++ specs/g.methods.spec.js
@@ -1,7 +1,7 @@
 var context = describe;
 
 context('[g Tests]', function(){
-  describe('g methods - ', function(){
+  describe('g methods, should be called - ', function(){
     var _g;
 
     beforeEach(function(){
@@ -29,6 +29,57 @@ context('[g Tests]', function(){
 
       expect(_g.after).toHaveBeenCalled();
     });
+
+    it('g.fill() must been called', function(){
+      spyOn(_g, 'fill');
+      _g.using("#template").fill('ul');
+
+      expect(_g.fill).toHaveBeenCalled();
+    });
+
+    it('g.append() must been called', function(){
+      spyOn(_g, 'append');
+      _g.using("#template").append('ul');
+
+      expect(_g.append).toHaveBeenCalled();
+    });
+
+    it('g.prepend() must been called', function(){
+      spyOn(_g, 'prepend');
+      _g.using("#template").prepend('ul');
+
+      expect(_g.prepend).toHaveBeenCalled();
+    });
+
+    it('g.before() must been called', function(){
+      spyOn(_g, 'before');
+      _g.using("#template").before('ul');
+
+      expect(_g.before).toHaveBeenCalled();
+    });
+
+
+
+  });
+
+  describe('g should fail when jquery and underscore are not included', function(){
+    var $$ = $;
+    var __ = _;
+
+    afterEach(function(){
+      $ = $$;
+      _ = __;
+    });
+
+   it('should fail when jquery is not defined', function(){
+     $ = undefined;
+     expect(function(){ g() }).toThrow('g(): You need jQuery, sorry man.');
+   });
+
+   it('should fail when underscore jquery is not defined', function(){
+     _ = undefined;
+     expect(function(){ g() }).toThrow('g(): You need Underscore, sorry man.');
+   });
 
   });
 });
